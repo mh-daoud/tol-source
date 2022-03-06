820d2a188c25060f008f1154bd9802baac3de05a
NOPA\MHDao <mh.daoud@hotmail.com>
2022-03-06 10:54:03 +0200

    initial commit to push ...

---
+++ TolSRC/PdfConverter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+using System.IO;
+
+namespace TolSRC
+{
+    public class PdfConverter
+    {
+        public string succsessMessage;
+        public string pdfAPIsPath = @"c:\pdfAPIs";
+
+        // Settings the density to 300 dpi will create an image with a better quality
+
+        public string PdfToImage(string pdfPath)
+        {
+            return PdfToImage(pdfPath, "", "", SortOrder.DES);
+        }
+
+        public string PdfToImage(string pdfPath, string destination)
+        {
+            return PdfToImage(pdfPath, destination, "", SortOrder.DES);
+        }
+
+        public string PdfToImage(string pdfPath, string destination, string namePattarn)
+        {
+            return PdfToImage(pdfPath, destination, namePattarn, SortOrder.DES);
+        }
+
+        public string PdfToImage(string pdfPath, string destination = "", string namePattarn = "", SortOrder sortOrder = SortOrder.DES)
+        {
+            try
+            {
+
+                
+                MagickNET.SetGhostscriptDirectory(pdfAPIsPath);
+                MagickReadSettings settings = new MagickReadSettings();
+                settings.Density = new Density(300, 300);
+                settings.Format = MagickFormat.Pdf;
+                
+                //Document document = new Document(file);
+                //RenderingSettings settings = new RenderingSettings();
+
+                string fileName = "image-from-pdf";
+                string imagesDestination = Path.GetDirectoryName(pdfPath) + @"\Done";
+                if (!string.IsNullOrEmpty(namePattarn))
+                {
+                    fileName = namePattarn;
+                }
+                if (!string.IsNullOrEmpty(destination))
+                {
+                    imagesDestination = destination;
+                }
+                using (MagickImageCollection images = new MagickImageCollection())
+                {
+                    // Add all the pages of the pdf file to the collection
+                    images.Read(pdfPath, settings);
+                    int i = 0;
+                    if (sortOrder == SortOrder.DES)
+                        i = images.Count - 1;
+                    if (!Directory.Exists(imagesDestination))
+                    {
+                        Directory.CreateDirectory(imagesDestination);
+                    }
+                    for (; ; )
+                    {
+                        MagickImage image = (MagickImage)images[i];
+                        //Page currentPage = document.Pages[i];
+                        //using (Bitmap bitmap = currentPage.Render((int)currentPage.Width, (int)currentPage.Height, settings))
+                        //{
+                        //    //bitmap.Save(Path.Combine(imagesDestination, fileName + i + ".png"));
+                        //    bitmap.Save(string.Format("{0}.png", @"" + imagesDestination + @"\" + fileName.Replace(" ", "-") + i), ImageFormat.Jpeg);
+
+
+                        //}
+
+                        // Write page to file that contains the page number
+                        //image.Write("Snakeware.Page" + page + ".png");
+                        // Writing to a specific format works the same as for a single image
+                        string fixedFileName = fileName;
+                        if(i < 10)
+                        {
+                            fixedFileName += "00";
+                        }
+                        else if (i < 100)
+                        {
+                            fixedFileName += "0";
+                        }
+                        string imageName = @"" + imagesDestination + @"\" + fixedFileName.Replace(" ", "-") + i + ".jpg";
+                        image.Write(imageName);
+                        if (sortOrder == SortOrder.DES)
+                        {
+                            i--;
+                            if (i < 0)
+                                break;
+                        }
+                        else
+                        {
+                            i++;
+                            if (i >= images.Count)
+                                break;
+                        }
+
+
+                    }
+                }
+                return succsessMessage;
+            }
+
+            catch (Exception e)
+            {
+                return e.ToString();
+
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ TolSRC/Renamer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace TolSRC
+{
+    public class Renamer
+    {
+
+        public bool StartRenamer(string sourceFolder, string destinationFolder, SortOrder sort, string namePattern=null)
+        {
+            if (string.IsNullOrEmpty(destinationFolder))
+            {
+                destinationFolder = @"" + sourceFolder + @"\Done";
+            }
+            
+            string[] files = Directory.EnumerateFiles(sourceFolder).OrderBy(filename => filename).ToArray();
+            if (!Directory.Exists(destinationFolder))
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+            int startCounter, endCounter;
+            if (sort == SortOrder.ASD)
+            {
+                startCounter = files.Length - 1;
+                endCounter = 0;
+            }
+            else
+            {
+                endCounter = files.Length - 1;
+                startCounter = 0;
+            }
+            if (sort == SortOrder.ASD)
+            {
+                for (; startCounter >= endCounter; startCounter--)
+                {
+                    //string extension = files[startCounter].Substring(files[startCounter].Length - 4);
+                    string extension = files[startCounter].Split('.')[1];
+                    string fileName = !string.IsNullOrEmpty(namePattern) ? namePattern : Path.GetFileName(files[startCounter]);
+                    fileName = fileName.Contains('.') ? fileName.ToString().Replace(extension, string.Empty).Split('.')[1] : fileName.ToString();
+                    string sufx = "";
+
+                    if (startCounter  < 100)
+                    {
+                        if (startCounter < 10)
+                            sufx = "00" + startCounter;
+                        else
+                            sufx = "0" + startCounter;
+                    }
+                    else
+                    {
+                        sufx = startCounter.ToString();
+                    }
+                    fileName = fileName.Substring(0, fileName.Length - 3) + (fileName.Contains("-") ? "" : "-")  + sufx + extension;
+
+                    string targetPath = Path.Combine(destinationFolder, fileName);
+                    CopyFile(Path.Combine(sourceFolder, files[startCounter]), targetPath);
+
+                }
+            }
+            else
+            {
+                for (; startCounter <= endCounter; startCounter++)
+                {
+                    string extension = files[startCounter].Split('.')[1];
+                    string fileName = !string.IsNullOrEmpty(namePattern) ? namePattern : Path.GetFileName(files[startCounter]);
+                    fileName = fileName.Contains('.') ? fileName.ToString().Replace(extension, string.Empty).Split('.')[1] : fileName.ToString();
+                    string sufx = "";
+
+                    if (endCounter - startCounter < 100)
+                    {
+                        if (endCounter - startCounter < 10)
+                            sufx = "00" + (endCounter - startCounter).ToString();
+                        else
+                            sufx = "0" + (endCounter - startCounter).ToString();
+                    }
+                    else
+                    {
+                        sufx = (endCounter - startCounter).ToString();
+                    }
+                    fileName = fileName + sufx  + "."+ extension;
+
+                    string targetPath = Path.Combine(destinationFolder, fileName);
+                    CopyFile(Path.Combine(sourceFolder, files[startCounter]), targetPath);
+
+                }
+            }
+            return true;
+        }
+
+
+        public void CopyFile(string sourceFolder, string destinationFolder)
+        {
+            try
+            {
+                File.Copy(sourceFolder, destinationFolder);
+            }
+            catch (Exception e)
+            {
+                
+                throw e;
+            }
+        }
+
+        public bool StartRenamer(string sourceFolder)
+        {
+            return StartRenamer(sourceFolder, "", SortOrder.DES);
+        }
+
+    }
+}
+
+public enum SortOrder
+{
+    ASD = 1,
+    DES = 2,
+}
